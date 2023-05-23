06362aad455b6bdcdb1c7e9af1ef66f5c86cb14a
comrider <cena3799@gmail.com>
2023-05-23 14:21:03 +0530

    bug fix for jenkins:ubuntu:22.04 :

--- Script/ubuntu/22.04/jenkins.sh
+++ Script/ubuntu/22.04/jenkins.sh
@@ -10,10 +10,13 @@ java -version
 sudo apt install default-jdk -y
 javac -version
 # installing jenkins 
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
-sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt update
-sudo apt install jenkins -y 
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
 # starting jenkins
 sudo systemctl start jenkins.service
 sudo systemctl status jenkins
