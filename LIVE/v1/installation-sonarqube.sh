1ba22f5ea8bacdd0367778e75923bca714d32c4b
comrider <cena3799@gmail.com>
2023-05-25 07:45:56 +0530

    sonarqube configuration file added

--- LIVE/v1/installation-sonarqube.sh
+++ LIVE/v1/installation-sonarqube.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 '''
- author : amal 
- use: for sonarcube 
+ author           : amal 
+ operation system : ubuntu:22.04
+ configuration -
+    cloud         : AWS
+    family        : t2.large 
+    cpu           : 2VPC
+    memory        : 8GiB memory 
+ use              : for sonarcube 
 '''
 # docker installation
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
@@ -22,9 +28,12 @@ sudo sysctl -w vm.max_map_count=524288
 sudo sysctl -w fs.file-max=131072
 ulimit -n 131072
 ulimit -u 8192
+'''
 sudo docker volume create data
 sudo docker volume create logs
 sudo docker volume create extensions 
 sudo chown $USER:$USER /var/run/docker.sock
-docker container run -d -p 9000:9000  --name sonar-qube -v data:/opt/sonarqube/data  -v logs:/opt/sonarqube/logs -v extension:/opt/sonarqube/extensions sonarqube 
-# default password: admin, new password: sonar
+docker container run -d -p 9000:9000  --name sonar-qube -v data:/opt/sonarqube/data  -v logs:/opt/sonarqube/logs -v extension:/opt/sonarqube/extensions sonarqube
+# default password: admin, new password: sonar 
+'''
+docker-compose -f sonar-docker-compose.yaml up -d
