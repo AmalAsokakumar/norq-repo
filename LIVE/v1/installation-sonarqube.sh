9639f2e86da56119ed8391d412fd4b83289aa52f
comrider <cena3799@gmail.com>
2023-05-25 08:05:58 +0530

    Docker-compose file for sonarQube is added

--- LIVE/v1/installation-sonarqube.sh
+++ LIVE/v1/installation-sonarqube.sh
@@ -28,6 +28,8 @@ sudo sysctl -w vm.max_map_count=524288
 sudo sysctl -w fs.file-max=131072
 ulimit -n 131072
 ulimit -u 8192
+
+'''
 # remove this segment when using docker compose file 
 sudo docker volume create data
 sudo docker volume create logs
@@ -35,10 +37,10 @@ sudo docker volume create extensions
 sudo chown $USER:$USER /var/run/docker.sock
 docker container run -d -p 9000:9000  --name sonar-qube -v data:/opt/sonarqube/data  -v logs:/opt/sonarqube/logs -v extension:/opt/sonarqube/extensions sonarqube
 # default password: admin, new password: sonar 
-
 '''
+
 # for docker compose 
 # curl -O -L <raw_file_url>
+curl -O -L https://raw.githubusercontent.com/AmalAsokakumar/norq-repo/main/LIVE/v1/sonar-docker-compose.yaml
 # wget <raw_file_url>
 docker-compose -f sonar-docker-compose.yaml up -d
-'''
