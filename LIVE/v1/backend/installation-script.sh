890f298c5b855d93697126888f4250ce803fe246
comrider <cena3799@gmail.com>
2023-05-28 00:09:17 +0530

    option on jenkins pipeline is removed

--- LIVE/v1/backend/installation-script.sh
+++ LIVE/v1/backend/installation-script.sh
@@ -13,7 +13,7 @@ sudo apt-get install -y nodejs
 node -v
 # installing mongodb 
 sudo apt update 
-sudo apt install wget curl gnupg2 software-properties-common apt-transport-https ca-certificates lsb-release
+sudo apt install -y wget curl gnupg2 software-properties-common apt-transport-https ca-certificates lsb-release 
 curl -fsSL https://www.mongodb.org/static/pgp/server-6.0.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/mongodb-6.gpg
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 sudo apt update 
@@ -22,7 +22,7 @@ sudo systemctl enable --now mongod
 sudo systemctl status mongod
 mongod --version
 # enable remote connection to mongoDB > go to the reference link for that.
-# install jenkins
+
 # creating a user
 adduser jenkins
 usermod -aG sudo jenkins 
